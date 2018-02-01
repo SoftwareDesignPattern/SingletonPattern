bee7de2ce65d7c862face7a2e57c531923dc1bd0
shahedbd <shahed.mbstu@gmail.com>
2018-02-01 17:35:47 +0600

    + Project code added

---
+++ Sln.SingletonPattern/SingletonPattern/GlobalVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public static class GlobalVariables
+    {
+        public static string strFilesLocs = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+        public static string strFilesLocsWithFilesName = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "\\Files\\F1.txt";
+
+        public static string strOutPutResult = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())) + "\\OutPut\\Result01.txt";
+    }
+}
